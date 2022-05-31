559a66d70b8c58b76cddf36dc02bdc9e81839739
Aurelien <91470700+THEGATOR98@users.noreply.github.com>
2022-05-31 16:52:02 +0200

    Update metiers_modele.h

--- metiers_modele.h
+++ metiers_modele.h
@@ -1,2 +1,34 @@
-// Oskar, Sacha
-// Aurelien
+#include <mysql.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void finish_with_error(MYSQL *con)
+{
+    fprintf(stderr, "%s\n", mysql_error(con));
+    mysql_close(con);
+    exit(1);
+}
+
+int main(int argc, char **argv)
+{
+    MYSQL *con = mysql_init(NULL);
+
+    if (con == NULL)
+    {
+        fprintf(stderr, "%s\n", mysql_error(con));
+        exit(1);
+    }
+
+    if (mysql_real_connect(con, "localhost", "dba", "ghjk", "celebrite", 0, NULL, 0) == NULL)
+    {
+        finish_with_error(con);
+    }
+
+    if (mysql_query(con, "INSERT INTO celebrites(metiers) VALUES('scientifique')"))
+    {
+        finish_with_error(con);
+    }
+
+    mysql_close(con);
+    exit(0);
+}
